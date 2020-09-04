55a521ac371b2c4ceec31f1f3496ff95a5fb5da4
Andres Mauricio Maca Hurtado <amaca@veritran.com>
2020-09-04 17:26:34 -0500

    init

---
+++ app/src/main/java/com/amaca/webviewpinning/MainActivity.java
@@ -0,0 +1,34 @@
+package com.amaca.webviewpinning;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.net.http.SslError;
+import android.os.Bundle;
+import android.webkit.SslErrorHandler;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //TrustKit.initializeWithNetworkSecurityConfiguration(this);
+
+        WebView webView = new WebView(this);
+        setContentView(webView);
+        webView.setWebChromeClient(new WebChromeClient());
+        webView.setWebViewClient(new WebViewClient(){
+            @Override
+            public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+                super.onReceivedSslError(view, handler, error);
+            }
+        });
+        webView.getSettings().setJavaScriptEnabled(true);
+
+        String urlComplete = "https://www.eltiempo.com";
+        webView.loadUrl(urlComplete);
+    }
+}
